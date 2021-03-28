b7a7f9b61b65e45ca172030628ff8e22eda80177
Sarah Lane <sarahlane8@gmail.com>
2021-03-28 16:12:02 -0600

    Add initial html and scripts formatting

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+const header = document.getElementsByTagName('header');
+const userInfoCard = document.getElementById('userInfoCard');
+const userStepGoals = document.getElementById('userStepGoals');
+const userRepository = new userRepository()
+//get access to data
+const user = new User(data)
+//event listeners
+//on page load, takes a user and displays their name in header accessing user class
+//on page load, takes a user and displays their step userStepGoals
+//function window.addEventListener('load', methodName)
