54d4a3156dee7dcd7ddf914550e80b3c96318575
Sarah Lane <sarahlane8@gmail.com>
2021-03-28 14:06:44 -0600

    Add beforeEach to test file

--- test/userRepository-test.js
+++ test/userRepository-test.js
@@ -1,79 +1,30 @@
 const chai = require('chai');
 const expect = chai.expect;
 const UserRepository = require('../src/userRepository.js');
-// const userData = require('../data/users.js');
 
 describe('UserRepository', () => {
+  let data;
+  let userRepository;
 
-it('should be a function', () => {
-  const userRepository = new UserRepository();
-
-  expect(UserRepository).to.be.a('function');
-})
-
-it('should instantiate a new UserRepository', () => {
-  const userRepository = new UserRepository();
-
-  expect(userRepository).to.be.an.instanceof(UserRepository);
-})
-
-it('should be able to take in an argument', () => {
-  const data = [{
-    "id": 150,
-    "name": "Marceline Lane",
-    "address": "1000 Main Street, Denver, CO 55555",
-    "email": "MarcelineLane@madeup.com",
-    "strideLength": 8,
-    "dailyStepGoal": 8000,
-    "friends": [
-      1,
-      2,
-      3
-    ]
-  }]
-
-
-  const userRepository = new UserRepository(data);
-
-  expect(userRepository.users[0].name).to.equal('Marceline Lane');
-})
-
-  it('should have a method that takes in a user\'s ID and returns that user\'s data', () => {
-    const data = [{
-      "id": 150,
-      "name": "Marceline Lane",
-      "address": "1000 Main Street, Denver, CO 55555",
-      "email": "MarcelineLane@madeup.com",
-      "strideLength": 8,
-      "dailyStepGoal": 8000,
-      "friends": [
-        1,
-        2,
-        3
-      ]
-    },
-    {
-      "id": 375,
-      "name": "Sarah Ball",
-      "address": "2000 Division Ave, Denver, CO 55555",
-      "email": "SarahBall@madeup.com",
-      "strideLength": 4.5,
-      "dailyStepGoal": 10000,
-      "friends": [
-        4,
-        5,
-        6
-      ]
-    }]
-    const userRepository = new UserRepository(data);
-
-    userRepository.findUserData(375);
-
-    expect(userRepository.findUserData(375)).to.deep.equal(  {
+  beforeEach(() => {
+    data = [{
+        "id": 150,
+        "name": "Marceline Lane",
+        "address": "1000 Main Street, Denver, CO 55555",
+        "email": "MarcelineLane@madeup.com",
+        "strideLength": 8,
+        "dailyStepGoal": 8000,
+        "friends": [
+          1,
+          2,
+          3
+        ]
+      },
+      {
         "id": 375,
-        "name": "Sarah Ball",
+        "name": "Margaret Mitchell",
         "address": "2000 Division Ave, Denver, CO 55555",
-        "email": "SarahBall@madeup.com",
+        "email": "MargaretMitchell@madeup.com",
         "strideLength": 4.5,
         "dailyStepGoal": 10000,
         "friends": [
@@ -81,28 +32,35 @@ it('should be able to take in an argument', () => {
           5,
           6
         ]
-      })
+      }
+    ]
+    userRepository = new UserRepository(data);
   })
 
-  it('should have a method that finds the average step goal of all users', () => {
-    const data = [{
-      "id": 150,
-      "name": "Marceline Lane",
-      "address": "1000 Main Street, Denver, CO 55555",
-      "email": "MarcelineLane@madeup.com",
-      "strideLength": 8,
-      "dailyStepGoal": 8000,
-      "friends": [
-        1,
-        2,
-        3
-      ]
-    },
-    {
+  it('should be a function', () => {
+
+    expect(UserRepository).to.be.a('function');
+  })
+
+  it('should instantiate a new UserRepository', () => {
+
+    expect(userRepository).to.be.an.instanceof(UserRepository);
+  })
+
+  it('should be able to take in an argument', () => {
+
+    expect(userRepository.users[0].name).to.equal('Marceline Lane');
+  })
+
+  it('should have a method that takes in a user\'s ID and returns that user\'s data', () => {
+
+    userRepository.findUserData(375);
+
+    expect(userRepository.findUserData(375)).to.deep.equal({
       "id": 375,
-      "name": "Sarah Ball",
+      "name": "Margaret Mitchell",
       "address": "2000 Division Ave, Denver, CO 55555",
-      "email": "SarahBall@madeup.com",
+      "email": "MargaretMitchell@madeup.com",
       "strideLength": 4.5,
       "dailyStepGoal": 10000,
       "friends": [
@@ -110,8 +68,10 @@ it('should be able to take in an argument', () => {
         5,
         6
       ]
-    }]
-    const userRepository = new UserRepository(data);
+    })
+  })
+
+  it('should have a method that finds the average step goal of all users', () => {
 
     expect(userRepository.findAverageStepGoal()).to.equal(9000);
   })
