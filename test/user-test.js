314cc34cb1a04c8c48e5fa5347d1e148cdbf74f9
Marceline Ball <marchettiball@gmail.com>
2021-03-28 14:57:41 -0600

    Add user test to take in data

--- test/user-test.js
+++ test/user-test.js
@@ -16,5 +16,22 @@ describe('User', () => {
     expect(user).to.be.an.instanceof(User);
   })
 
-  
+  it('should be able to take in userData object', () => {
+    const data = {
+        "id": 150,
+        "name": "Marceline Lane",
+        "address": "1000 Main Street, Denver, CO 55555",
+        "email": "MarcelineLane@madeup.com",
+        "strideLength": 8,
+        "dailyStepGoal": 8000,
+        "friends": [
+          1,
+          2,
+          3
+        ]
+      };
+    const user = new User(data);
+
+    expect(user.id).to.equal(150);
+  })
 })
