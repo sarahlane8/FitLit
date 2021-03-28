dfc2b0708525d9f3e4ed975e358579e64e717318
Sarah Lane <sarahlane8@gmail.com>
2021-03-28 13:59:19 -0600

    Add findAverageStepGoal method

--- src/userRepository.js
+++ src/userRepository.js
@@ -1,12 +1,18 @@
 class UserRepository {
   constructor(data) {
-    this.users = data;//array of 50 objects
+    this.users = data;
   }
   findUserData(userId) {
     const foundUser = this.users.find(user => user.id === userId);
     return foundUser;
   }
-  //findAverageStepGoal()
+  findAverageStepGoal() {
+    let avgStepGoal = this.users.reduce((acc, user) => {
+      return acc + user.dailyStepGoal
+    }, 0)
+    return avgStepGoal / this.users.length;
+  }
 }
+
 
 module.exports = UserRepository;
