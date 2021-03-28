ce0e21bc2e5ee0452cccaa6cd796805f9a48d61a
Sarah Lane <sarahlane8@gmail.com>
2021-03-28 12:47:47 -0600

    Write test for instantiating new UserRepository

--- src/userRepository.js
+++ src/userRepository.js
@@ -1,1 +1,7 @@
+class UserRepository {
+  constructor() {
+
+  }
+}
+
 module.exports = UserRepository;
--- test/userRepository-test.js
+++ test/userRepository-test.js
@@ -1,3 +1,39 @@
 const chai = require('chai');
-const expect = require('chai.expect');
+const expect = chai.expect;
 const UserRepository = require('../src/userRepository.js');
+// const userData = require('../data/users.js');
+
+describe('UserRepository', () => {
+
+it('should be a function', () => {
+  const userRepository = new UserRepository();
+
+  expect(UserRepository).to.be.a('function');
+})
+
+it('should instantiate a new UserRepository', () => {
+  const userRepository = new UserRepository();
+
+  expect(userRepository).to.be.an.instanceof(UserRepository);
+})
+
+it('should be able to take in an argument', () => {
+  const data = {
+    "id": 150,
+    "name": "Marceline Lane",
+    "address": "1000 Main Street, Denver, CO 55555",
+    "email": "MarcelineLane@madeup.com",
+    "strideLength": 8,
+    "dailyStepGoal": 8000,
+    "friends": [
+      1,
+      2,
+      3
+    ]
+  }
+  const userRepository = new UserRepository(data);
+
+  expect(data.name).to.equal('Marceline Lane');
+})
+
+})
