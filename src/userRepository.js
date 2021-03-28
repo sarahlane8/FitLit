3be362c9860369cd39222fee691afe086b5e2cf0
Sarah Lane <sarahlane8@gmail.com>
2021-03-28 12:58:15 -0600

    Add findUserData test

--- src/userRepository.js
+++ src/userRepository.js
@@ -1,7 +1,8 @@
 class UserRepository {
-  constructor() {
-
+  constructor(data) {
+    this.users = data;//array of 50 objects
   }
+  //method that .finds a users ID and returns the object
 }
 
 module.exports = UserRepository;
--- test/userRepository-test.js
+++ test/userRepository-test.js
@@ -18,7 +18,7 @@ it('should instantiate a new UserRepository', () => {
 })
 
 it('should be able to take in an argument', () => {
-  const data = {
+  const data = [{
     "id": 150,
     "name": "Marceline Lane",
     "address": "1000 Main Street, Denver, CO 55555",
@@ -30,10 +30,58 @@ it('should be able to take in an argument', () => {
       2,
       3
     ]
-  }
+  }]
+
+
   const userRepository = new UserRepository(data);
 
-  expect(data.name).to.equal('Marceline Lane');
+  expect(userRepository.users[0].name).to.equal('Marceline Lane');
 })
+
+  it('should have a method that takes in a user\'s ID and returns that user\'s data', () => {
+    const data = [{
+      "id": 150,
+      "name": "Marceline Lane",
+      "address": "1000 Main Street, Denver, CO 55555",
+      "email": "MarcelineLane@madeup.com",
+      "strideLength": 8,
+      "dailyStepGoal": 8000,
+      "friends": [
+        1,
+        2,
+        3
+      ]
+    },
+    {
+      "id": 375,
+      "name": "Sarah Ball",
+      "address": "2000 Division Ave, Denver, CO 55555",
+      "email": "SarahBall@madeup.com",
+      "strideLength": 4.5,
+      "dailyStepGoal": 10000,
+      "friends": [
+        4,
+        5,
+        6
+      ]
+    }]
+    const userRepository = new UserRepository(data);
+
+    userRepository.findUserData(375);
+
+    expect(userRepository.findUserData(375)).to.deep.equal(  {
+        "id": 375,
+        "name": "Sarah Ball",
+        "address": "2000 Division Ave, Denver, CO 55555",
+        "email": "SarahBall@madeup.com",
+        "strideLength": 4.5,
+        "dailyStepGoal": 10000,
+        "friends": [
+          4,
+          5,
+          6
+        ]
+      })
+  })
 
 })
