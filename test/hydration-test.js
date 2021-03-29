56daf6b80fb6a53b37471c4754d44f32afb63001
Marceline Ball <marchettiball@gmail.com>
2021-03-29 16:22:28 -0600

    Add intialization tests for hydration

--- test/hydration-test.js
+++ test/hydration-test.js
@@ -1,3 +1,39 @@
 const chai = require('chai');
 const expect = chai.expect;
 const Hydration = require('../src/Hydration');
+
+describe('hydration', () => {
+  let hydrationData
+  let hydration
+
+  beforeEach(() => {
+    hydrationData = [
+      {
+        "userID": 1,
+        "date": "2019/06/15",
+        "numOunces": 37
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/18",
+        "numOunces": 53
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/20",
+        "numOunces": 41
+      }
+    ]
+    hydration = new Hydration()
+  })
+
+  it('should be a function', () => {
+
+    expect(Hydration).to.be.a('function');
+  })
+
+  it('should be an instance of Hydration', () => {
+
+    expect(hydration).to.be.an.instanceof(Hydration);
+  })
+})
