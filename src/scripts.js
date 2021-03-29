45f43626b1e1320045adeab0c0a3825a1f467f77
Sarah Lane <sarahlane8@gmail.com>
2021-03-29 14:51:29 -0600

    Add displayUserCardInfo method functionality

--- src/scripts.js
+++ src/scripts.js
@@ -10,15 +10,25 @@ window.addEventListener('load', manageLoadingFunctions)
 
 function manageLoadingFunctions() {
   displayUserGreeting(user1);
-  displayUserInfoCard();
-  displayUserStepGoal()
+  displayUserInfoCard(user1);
+  displayUserStepGoal(user1)
 }
 
 function displayUserGreeting(user) {
   header.innerHTML = `<h1>Hello, ${user1.findUserFirstName()}</h1>`
 }
 
-function displayUserInfoCard() {
+function displayUserInfoCard(user) {
+  userInfoCard.innerHTML =
+`
+<h3>${user.name}</h3>
+<p>Address: ${user.address}</p>
+<p>Daily Step Goal: ${user.dailyStepGoal}</p>
+<p>Email: ${user.email}</p>
+<p>You are friends with: </p>
+<p>Your stride length: ${user.strideLength}</p>
+
+`
 
 }
 
