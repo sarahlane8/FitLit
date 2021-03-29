540a61ca6353821423bac6fb02dfe8a7255e925f
Sarah Lane <sarahlane8@gmail.com>
2021-03-29 14:43:23 -0600

    Add functionality to displayUserGreeting method

--- src/scripts.js
+++ src/scripts.js
@@ -1,12 +1,29 @@
-const header = document.getElementsByTagName('header');
+const header = document.getElementById('header');
 const userInfoCard = document.getElementById('userInfoCard');
 const userStepGoals = document.getElementById('userStepGoals');
 const userRepository = new UserRepository()
-const user = new User(userData[0])
+const user1 = new User(userData[0])
 
 //event listeners
+window.addEventListener('load', manageLoadingFunctions)
 
 
+function manageLoadingFunctions() {
+  displayUserGreeting(user1);
+  displayUserInfoCard();
+  displayUserStepGoal()
+}
+
+function displayUserGreeting(user) {
+  header.innerHTML = `<h1>Hello, ${user1.findUserFirstName()}</h1>`
+}
+
+function displayUserInfoCard() {
+
+}
+
+function displayUserStepGoal() {
+
+}
 //on page load, takes a user and displays their name in header accessing user class
 //on page load, takes a user and displays their step userStepGoals
-//function window.addEventListener('load', methodName)
