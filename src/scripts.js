f149c7b067ff3a5318a58f1c08eb84d2cf805f9b
Sarah Lane <sarahlane8@gmail.com>
2021-03-29 14:27:53 -0600

    Add class declarations

--- src/scripts.js
+++ src/scripts.js
@@ -1,10 +1,12 @@
 const header = document.getElementsByTagName('header');
 const userInfoCard = document.getElementById('userInfoCard');
 const userStepGoals = document.getElementById('userStepGoals');
-const userRepository = new userRepository()
-//get access to data
-const user = new User(data)
+const userRepository = new UserRepository()
+const user = new User(userData[0])
+
 //event listeners
+
+
 //on page load, takes a user and displays their name in header accessing user class
 //on page load, takes a user and displays their step userStepGoals
 //function window.addEventListener('load', methodName)
