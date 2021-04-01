fc0413505991e480a29de84198461571a1e852b5
Marceline Ball <marchettiball@gmail.com>
2021-04-01 16:03:04 -0600

    Add avg sleep quality method to class

--- src/sleepRepository.js
+++ src/sleepRepository.js
@@ -2,6 +2,14 @@ class SleepRepository {
   constructor(sleepData) {
     this.sleepData = sleepData
   }
+
+  findAverageSleepQuality() {
+    const combinedSleepQuality = this.sleepData.reduce((acc, userObject) => {
+      return acc += userObject.sleepQuality
+    }, 0)
+    const averageQuality = (combinedSleepQuality / this.sleepData.length)
+    return Math.round(averageQuality * 10) / 10
+  }
 }
 
 if (typeof module !== 'undefined') {
