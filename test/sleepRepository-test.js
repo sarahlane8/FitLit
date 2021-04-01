7821ada573bd6436aa453dfe1543fa1f33d6da50
Marceline Ball <marchettiball@gmail.com>
2021-04-01 15:28:57 -0600

    Add initialization tests to test file

--- test/sleepRepository-test.js
+++ test/sleepRepository-test.js
@@ -1,3 +1,16 @@
 const chai = require('chai');
 const expect = chai.expect;
 const SleepRepository = require('../src/sleepRepository');
+
+describe('sleepRepository', () => {
+
+  it('should be a function', () => {
+
+    expect(SleepRepository).to.be.a('function');
+  })
+
+  it('should be an instance of the SleepRepository class', () => {
+
+    expect(sleepRepository).to.be.an.instanceof(SleepRepository);
+  })
+})
