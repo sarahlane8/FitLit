64a7d2b3ea5d006e8f7a45dd1913428b58975473
Marceline Ball <marchettiball@gmail.com>
2021-04-01 15:43:51 -0600

    Add before each and constructor test

--- test/sleepRepository-test.js
+++ test/sleepRepository-test.js
@@ -2,7 +2,34 @@ const chai = require('chai');
 const expect = chai.expect;
 const SleepRepository = require('../src/sleepRepository');
 
-describe('sleepRepository', () => {
+describe('SleepRepository', () => {
+  let sleepData
+  let sleepRepository
+
+  beforeEach(() => {
+    sleepData = [
+      {
+        "userID": 1,
+        "date": "2019/06/15",
+        "hoursSlept": 8.1,
+        "sleepQuality": 3.2
+      },
+      {
+        "userID": 2,
+        "date": "2019/06/15",
+        "hoursSlept": 9,
+        "sleepQuality": 4.4
+      },
+      {
+        "userID": 3,
+        "date": "2019/06/15",
+        "hoursSlept": 1.2,
+        "sleepQuality": 4
+      }
+    ];
+
+    sleepRepository = new SleepRepository(sleepData)
+  })
 
   it('should be a function', () => {
 
@@ -10,8 +37,12 @@ describe('sleepRepository', () => {
   })
 
   it('should be an instance of the SleepRepository class', () => {
-    const sleepRepository = new SleepRepository();
 
     expect(sleepRepository).to.be.an.instanceof(SleepRepository);
+  })
+
+  it('should be able to take in sleep data', () => {
+
+    expect(sleepRepository[0]).to.deep.equal(sleepData[0]);
   })
 })
