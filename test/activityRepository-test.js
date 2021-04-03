0125e8336f0efee42c78b862738f8e5cdaed6c46
Sarah Lane <sarahlane8@gmail.com>
2021-04-03 17:56:31 -0600

    Add beforeEach method to activityRepository test file

--- test/activityRepository-test.js
+++ test/activityRepository-test.js
@@ -1,3 +1,15 @@
 const chai = require('chai');
 const expect = chai.expect;
 const ActivityRepository = require('../src/activityRepository')
+
+describe('ActivityRepository', () => {
+  let activityRepository;
+  let activityData = {
+
+  }
+  beforeEach(() => {
+    activityRepository = new ActivityRepository(activityData);
+  })
+
+
+})
