55a862d5c8366e0d786b4264fb84d3192a8cd991
Marceline Ball <marchettiball@gmail.com>
2021-04-03 10:27:45 -0600

    Add before each to sleep test

--- test/sleep-test.js
+++ test/sleep-test.js
@@ -1,3 +1,26 @@
 const chai = require('chai');
-const sleep = require('../src/sleep');
+const Sleep = require('../src/sleep');
 const SleepRepository = require('../src/sleepRepository');
+
+describe('Sleep', () => {
+  const userSleepData = [
+    {
+      "userID": 1,
+      "date": "2019/06/15",
+      "hoursSlept": 8.1,
+      "sleepQuality": 2.9
+    },{
+      "userID": 1,
+      "date": "2019/06/30",
+      "hoursSlept": 7.4,
+      "sleepQuality": 3.2
+    }
+  ]
+  let sleep
+
+  beforeEach (() => {
+    sleep = new Sleep(userSleepData)
+  })
+
+  
+})
