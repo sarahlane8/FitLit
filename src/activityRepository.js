bfab93a18d8c3b8a32133d817cb2e83012ba32ce
Sarah Lane <sarahlane8@gmail.com>
2021-04-03 18:43:27 -0600

    Add changes from eslint

--- src/activityRepository.js
+++ src/activityRepository.js
@@ -3,12 +3,12 @@ class ActivityRepository {
     this.userData = activityData;
   }
 
-findAverageProperty(date, property) {
-  const usersByDate = this.userData.filter(user => user.date === date);
-  const averageProperty = usersByDate.reduce((acc, user) => {
-    return acc += user[property];
-  }, 0)
-  return Math.round(averageProperty / usersByDate.length);
+  findAverageProperty(date, property) {
+    const usersByDate = this.userData.filter(user => user.date === date);
+    const averageProperty = usersByDate.reduce((acc, user) => {
+      return acc += user[property];
+    }, 0)
+    return Math.round(averageProperty / usersByDate.length);
   }
 
   findActivityUserData(userId) {
