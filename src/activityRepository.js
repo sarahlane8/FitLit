96c2cea6e5795f4f3b82ffeb3d4f6331cd46ce53
Sarah Lane <sarahlane8@gmail.com>
2021-04-03 18:09:49 -0600

    Add method and test to take activityData as argument

--- src/activityRepository.js
+++ src/activityRepository.js
@@ -1,6 +1,6 @@
 class ActivityRepository {
-  constructor() {
-
+  constructor(activityData) {
+    this.userData = activityData;
   }
 }
 
--- test/activityRepository-test.js
+++ test/activityRepository-test.js
@@ -4,19 +4,44 @@ const ActivityRepository = require('../src/activityRepository')
 
 describe('ActivityRepository', () => {
   let activityRepository;
-  let activityData = {
+  const activityData = [{
+    "userID": 1,
+    "date": "2019/06/15",
+    "numSteps": 6377,
+    "minutesActive": 240,
+    "flightsOfStairs": 6
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/15",
+    "numSteps": 14264,
+    "minutesActive": 29,
+    "flightsOfStairs": 26
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/25",
+    "numSteps": 10002,
+    "minutesActive": 88,
+    "flightsOfStairs": 18
+  }];
 
-  }
   beforeEach(() => {
     activityRepository = new ActivityRepository(activityData);
   })
 
   it('should be a function', () => {
+
     expect(ActivityRepository).to.be.a('function');
   })
 
   it('should instantiate a new activityRepository', () => {
 
     expect(activityRepository).to.be.an.instanceof(ActivityRepository);
+  })
+
+  it('should take in activityData as an argument', () => {
+
+    expect(activityRepository.userData).to.deep.equal(activityData);
   })
 })
