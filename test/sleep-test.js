37eb955328f3fb5a2fa76078ede3e76a4a70c1cc
Sarah Lane <sarahlane8@gmail.com>
2021-04-04 19:40:13 -0600

    Refactor spacing and make changes from eslint

--- test/sleep-test.js
+++ test/sleep-test.js
@@ -1,7 +1,6 @@
 const chai = require('chai');
 const expect = chai.expect;
 const Sleep = require('../src/sleep');
-const SleepRepository = require('../src/sleepRepository');
 
 describe('Sleep', () => {
   const userSleepData = [
@@ -35,11 +34,11 @@ describe('Sleep', () => {
       "hoursSlept": 7.4,
       "sleepQuality": 3.2
     }
-  ]
-  let sleep
+  ];
+  let sleep;
 
   beforeEach (() => {
-    sleep = new Sleep(userSleepData)
+    sleep = new Sleep(userSleepData);
   })
 
   it('should be a function', () => {
@@ -75,40 +74,38 @@ describe('Sleep', () => {
 
   it('should be able to return hours slept for a particular date', () => {
 
-    expect(sleep.findSleepPropertyByDate('2019/06/15', 'hoursSlept')).to.equal(8.1)
+    expect(sleep.findSleepPropertyByDate('2019/06/15', 'hoursSlept')).to.equal(8.1);
   })
 
   it('should be able to return sleep quality for partucular date', () => {
 
-    expect(sleep.findSleepPropertyByDate('2019/06/15', 'sleepQuality')).to.equal(2.9)
+    expect(sleep.findSleepPropertyByDate('2019/06/15', 'sleepQuality')).to.equal(2.9);
   })
 
   it('should be able to return an array of sleep objects for a week when given a date', () => {
 
-    expect(sleep.filterSleepDataByWeek('2019/06/18')).to.deep.equal([userSleepData[0], userSleepData[1], userSleepData[2], userSleepData[3]])
+    expect(sleep.filterSleepDataByWeek('2019/06/18')).to.deep.equal([userSleepData[0], userSleepData[1], userSleepData[2], userSleepData[3]]);
   })
 
   it('should be able to return hours slept for each day for a given week', () => {
 
     expect(sleep.findDailySleepPropertyByWeek('2019/06/18', 'hoursSlept')).to.deep.equal
-      ({'2019/06/15': 8.1, '2019/06/16': 9.1, '2019/06/17': 6.4, '2019/06/18': 9.2})
+      ({'2019/06/15': 8.1, '2019/06/16': 9.1, '2019/06/17': 6.4, '2019/06/18': 9.2});
   })
 
   it('should return an empty object if no user data for a week exists', () => {
 
-    expect(sleep.findDailySleepPropertyByWeek('2021/06/18', 'hoursSlept')).to.deep.equal
-      ({})
+    expect(sleep.findDailySleepPropertyByWeek('2021/06/18', 'hoursSlept')).to.deep.equal({});
   })
 
   it('should be able to return sleep quality for each day of a given week', () => {
 
     expect(sleep.findDailySleepPropertyByWeek('2019/06/18', 'sleepQuality')).to.deep.equal
-      ({'2019/06/15': 2.9, '2019/06/16': 1.9, '2019/06/17': 2.5, '2019/06/18': 3.7})
+      ({'2019/06/15': 2.9, '2019/06/16': 1.9, '2019/06/17': 2.5, '2019/06/18': 3.7});
   })
 
   it('should return an empty object if no user data for a week exists', () => {
 
-    expect(sleep.findDailySleepPropertyByWeek('2021/06/18', 'sleepQuality')).to.deep.equal
-      ({})
+    expect(sleep.findDailySleepPropertyByWeek('2021/06/18', 'sleepQuality')).to.deep.equal({});
   })
 })
