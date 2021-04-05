1be62834e4bee8ebba039d966d21e0cf42dc1732
Sarah Lane <sarahlane8@gmail.com>
2021-04-05 14:55:50 -0600

    Add progress to sleep chart

--- src/scripts.js
+++ src/scripts.js
@@ -20,6 +20,7 @@ const userWeeklyActivityStats = document.getElementById('userWeeklyActivityStats
 const activityRepository = new ActivityRepository(activityData);
 const activity = new Activity(activityRepository.findActivityUserData(1));
 const waterChart = document.getElementById('waterChart');
+const sleepChart = document.getElementById('sleepChart');
 
 
 
@@ -95,21 +96,15 @@ function displayUserWeeklyWater(date) {
       data: chartData,
       backgroundColor: [
         { fillColor: 'rgba(255, 99, 132, 0.2)' }
-        // 'rgba(255, 159, 64, 0.2)',
-        // 'rgba(255, 205, 86, 0.2)',
-        // 'rgba(75, 192, 192, 0.2)',
-        // 'rgba(54, 162, 235, 0.2)',
-        // 'rgba(153, 102, 255, 0.2)',
-        // 'rgba(201, 203, 207, 0.2)'
       ],
       borderColor: [
-        'rgb(255, 99, 132)',
-        'rgb(255, 159, 64)',
-        'rgb(255, 205, 86)',
-        'rgb(75, 192, 192)',
-        'rgb(54, 162, 235)',
+        // 'rgb(255, 99, 132)',
+        // 'rgb(255, 159, 64)',
+        // 'rgb(255, 205, 86)',
+        // 'rgb(75, 192, 192)',
+        // 'rgb(54, 162, 235)',
         'rgb(153, 102, 255)',
-        'rgb(201, 203, 207)'
+        // 'rgb(201, 203, 207)'
       ],
       borderWidth: 1
     }]
@@ -142,6 +137,20 @@ function displayUserWeeklySleep(date) {
   `
   <h3>${userWeeklySleepData}</h3>
   `
+  const data = {
+    labels: labels,
+    datasets: [{
+      label: 'My First Dataset',
+      data: [65, 59, 80, 81, 56, 55, 40],
+      fill: false,
+      borderColor: 'rgb(75, 192, 192)',
+      tension: 0.1
+    }]
+  };
+  var myLineChart = new Chart(ctx, {
+      type: 'line',
+      data: data
+
 }
 
 function displayUserSleepAverage() {
