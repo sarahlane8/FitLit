35a9aacef834d52410eee0ff1162c4fe9aedb558
Sarah Lane <sarahlane8@gmail.com>
2021-04-07 17:06:27 -0600

    Add refactoring to UserRepository.js

--- src/userRepository.js
+++ src/userRepository.js
@@ -2,13 +2,15 @@ class UserRepository {
   constructor(data) {
     this.users = data;
   }
+
   findUserData(userId) {
     const foundUser = this.users.find(user => user.id === userId);
     return foundUser;
   }
+
   findAverageStepGoal() {
     let avgStepGoal = this.users.reduce((acc, user) => {
-      return acc + user.dailyStepGoal
+      return acc + user.dailyStepGoal;
     }, 0)
     return avgStepGoal / this.users.length;
   }
