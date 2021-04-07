719254f49cf8473a1ba3149f1649417d536eb0ee
Sarah Lane <sarahlane8@gmail.com>
2021-04-07 17:04:20 -0600

    Add refactoring to SleepRepository.js

--- src/sleepRepository.js
+++ src/sleepRepository.js
@@ -1,32 +1,32 @@
-// const dayjs = require('dayjs');
-// const isBetween = require('dayjs/plugin/isBetween');
-// dayjs.extend(isBetween);
+const dayjs = require('dayjs');
+const isBetween = require('dayjs/plugin/isBetween');
+dayjs.extend(isBetween);
 
 class SleepRepository {
   constructor(sleepData) {
-    this.sleepData = sleepData
+    this.sleepData = sleepData;
   }
 
   findSleepUserData(userId) {
-    return this.sleepData.filter(user => user.userID === userId)
+    return this.sleepData.filter(user => user.userID === userId);
   }
 
   findAverageSleepQuality() {
     const combinedSleepQuality = this.sleepData.reduce((acc, userObject) => {
-      return acc += userObject.sleepQuality
+      return acc += userObject.sleepQuality;
     }, 0)
-    const averageQuality = (combinedSleepQuality / this.sleepData.length)
-    return Math.round(averageQuality * 10) / 10
+    const averageQuality = (combinedSleepQuality / this.sleepData.length);
+    return Math.round(averageQuality * 10) / 10;
   }
 
   filterSleepDataByWeek(date) {
     const weekBeginningDate = dayjs(date).subtract(6, 'day');
     const weekEndingDate = dayjs(date);
-    return this.sleepData.filter(({date}) => dayjs(date).isBetween(weekBeginningDate, weekEndingDate, null, '[]'))
+    return this.sleepData.filter(({date}) => dayjs(date).isBetween(weekBeginningDate, weekEndingDate, null, '[]'));
   }
 
   findUsersWithQualitySleep(date) {
-    const weekOfSleepDataObjects = this.filterSleepDataByWeek(date)
+    const weekOfSleepDataObjects = this.filterSleepDataByWeek(date);
     const result = weekOfSleepDataObjects.reduce((acc, user) => {
       const { userID, hoursSlept = 0, sleepQuality = 0 } = user;
       if (!acc[userID]) {
@@ -34,13 +34,12 @@ class SleepRepository {
           hoursSum: hoursSlept,
           qualitySum: sleepQuality,
           numOfDays: 1
-        }
+        };
+      } else {
+        acc[userID].hoursSum += hoursSlept;
+        acc[userID].qualitySum += sleepQuality;
+        acc[userID].numOfDays++
       }
-
-      acc[userID].hoursSum += hoursSlept;
-      acc[userID].qualitySum += sleepQuality;
-      acc[userID].numOfDays++;
-
       return acc;
     }, {});
     return Object.keys(result)
@@ -49,19 +48,20 @@ class SleepRepository {
   }
 
   filterSleepDataByDate(date) {
-    return this.sleepData.filter(object => object.date === date)
+    return this.sleepData.filter(object => object.date === date);
   }
 
   findUserWhoSleptLongest(date) {
-    const usersByDate = this.filterSleepDataByDate(date)
+    const usersByDate = this.filterSleepDataByDate(date);
     usersByDate.sort((a, b) => {
-      return b.hoursSlept - a.hoursSlept
+      return b.hoursSlept - a.hoursSlept;
     })
     const mostHours = usersByDate[0].hoursSlept;
     const usersWithMostHours = usersByDate.filter(user => user.hoursSlept === mostHours);
-    return usersWithMostHours.map(user => user.userID)
+    return usersWithMostHours.map(user => user.userID);
   }
 }
+
 
 if (typeof module !== 'undefined') {
   module.exports = SleepRepository;
